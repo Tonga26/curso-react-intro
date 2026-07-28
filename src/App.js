85a15498bd0cc700b80dcf5f64e5acb7e9bb6346
Gaston Giorgio <gastongiorgio26@gmail.com>
2026-07-28 11:57:04 -0300

    refactor: implementación de props, renderizado de arrays con .map() y asignación de keys

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
+import React from 'react';
 import { TodoCounter } from './TodoCounter';
 import { TodoSearch } from './TodoSearch';
 import { TodoList } from './TodoList';
 import { TodoItem } from './TodoItem';
+import { CreateTodoButton } from './CreateTodoButton';
 import './App.css';
+
+const defaultTodos = [
+  { text:'Cortar cebollas', completed:false },
+  { text:'Tomar el curso de introduccion a React.js', completed:false },
+  { text:'Llorar con la llorona', completed:false },
+  { text:'Lalalalala', completed:false }
+]
 
 function App() {
   return (
-    <div className="App">
-
-      <TodoCounter />
+    <React.Fragment>
+      <TodoCounter completed={16} total={25} />
       <TodoSearch />
 
       <TodoList>
-        <TodoItem />
-        <TodoItem />
-        <TodoItem />
+        {defaultTodos.map( todo => (
+          <TodoItem 
+          key={todo.text} 
+          text={todo.text}
+          completed={todo.completed}
+          />
+        ))}
       </TodoList>
 
-      {/* <CreateTodoButton /> */}
-
-    </div>
+      <CreateTodoButton />
+    </React.Fragment>
   );
 }
 
