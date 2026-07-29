f931a5797e84b3d4d539f0f649d9d34830c797aa
Gaston Giorgio <gastongiorgio26@gmail.com>
2026-07-29 11:48:18 -0300

    style: actualización de estilos CSS y clases BEM según el diseño oficial del curso

--- src/TodoItem.js
+++ src/TodoItem.js
@@ -3,8 +3,8 @@ import './TodoItem.css';
 function TodoItem(props) {
   return (
     <li className="TodoItem">
-      <span className="Icon Icon-check">V</span>
-      <p className="TodoItem-p">{props.text}</p>
+      <span className="Icon Icon-check Icon Icon-check Icon-check--active">V</span>
+      <p className="TodoItem-p TodoItem-p--complete">{props.text}</p>
       <span className="Icon Icon-delete">X</span>
     </li>
   );
